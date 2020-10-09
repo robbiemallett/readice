beae768e7a15a6b078ac42d06a81f2f07db8868d
robbiemallett <robbiemallett@gmail.com>
2020-10-09 15:17:48 +0100

    travis

--- pip/setup.py
+++ pip/setup.py
@@ -1,16 +1,11 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="read_ice",
     version="0.0.1",
     author="Robbie Mallett",
+    packages=['read_ice'],
     author_email="robbie.mallett.17@ucl.co.uk",
-    description="A package for reading tricky polar data.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/robbiemallett/read_ice",
     install_requires=['pandas',
                       'numpy',
